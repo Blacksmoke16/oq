a9601a550729b2784d029edc624d01e61ea9b9cd
Blacksmoke16 <yomoejoe@gmail.com>
2019-08-05 11:15:08 -0400

    Revert Yaml.deserialize to allow for anchors/alises (#22)

--- src/converters/yaml.cr
+++ src/converters/yaml.cr
@@ -6,52 +6,9 @@ class JSON::Builder
 end
 
 module OQ::Converters::Yaml
-  # ameba:disable Metrics/CyclomaticComplexity
+  # OPTIMIZE: Figure out a way to handle aliases/anchors while streaming.
   def self.deserialize(input : IO, output : IO, **args) : Nil
-    yaml = YAML::PullParser.new(input)
-    json = JSON::Builder.new(output)
-
-    yaml.read_stream do
-      loop do
-        case yaml.kind
-        when .document_start?
-          json.start_document
-        when .document_end?
-          json.end_document
-          yaml.read_next
-          break
-        when .scalar?
-          string = yaml.value
-
-          if json.next_is_object_key?
-            json.scalar(string)
-          else
-            scalar = YAML::Schema::Core.parse_scalar(yaml)
-            case scalar
-            when Nil
-              json.scalar(scalar)
-            when Bool
-              json.scalar(scalar)
-            when Int64
-              json.scalar(scalar)
-            when Float64
-              json.scalar(scalar)
-            else
-              json.scalar(string)
-            end
-          end
-        when .sequence_start?
-          json.start_array
-        when .sequence_end?
-          json.end_array
-        when .mapping_start?
-          json.start_object
-        when .mapping_end?
-          json.end_object
-        end
-        yaml.read_next
-      end
-    end
+    YAML.parse(input).to_json output
   end
 
   # ameba:disable Metrics/CyclomaticComplexity
