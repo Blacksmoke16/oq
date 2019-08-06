9f50edc876de21421f73a66b3de5f6f72d5f3b9d
Blacksmoke16 <yomoejoe@gmail.com>
2019-08-06 08:19:26 -0400

    Release 0.2.0 (#23)

--- src/oq.cr
+++ src/oq.cr
@@ -6,7 +6,7 @@ require "./converters/*"
 
 # A performant and portable `jq` wrapper to support formats other than JSON.
 module OQ
-  VERSION = "0.1.1"
+  VERSION = "0.2.0"
 
   # The support formats that can be converted to/from.
   enum Format
