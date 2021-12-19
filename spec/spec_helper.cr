f1556705404399c755037407c89cd6389eda60e9
George Dietrich <george@dietrich.app>
2021-12-19 13:09:02 -0500

    Use empty IO on `null` input (#102)
    * Use empty IO on `null` input
    * Bump versions

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,12 +2,19 @@ require "spec"
 require "../src/oq"
 
 # Runs the the binary with the given *name* and *args*.
-def run_binary(input : String? = nil, name : String = "bin/oq", args : Array(String) = [] of String, *, success : Bool = true, file = __FILE__, line = __LINE__, & : String, Process::Status, String -> Nil)
+def run_binary(input : String | Process::Redirect | Nil = nil, name : String = "bin/oq", args : Array(String) = [] of String, *, success : Bool = true, file = __FILE__, line = __LINE__, & : String, Process::Status, String -> Nil)
   buffer_io = IO::Memory.new
   error_io = IO::Memory.new
   input_io = IO::Memory.new
-  input_io << input if input
-  status = Process.run(name, args, output: buffer_io, input: input_io.rewind, error: error_io)
+
+  if input.is_a? Process::Redirect
+    input_io = input
+  else
+    input_io << input if input
+    input_io = input_io.rewind
+  end
+
+  status = Process.run(name, args, output: buffer_io, input: input_io, error: error_io)
 
   if success
     status.success?.should be_true, file: file, line: line
